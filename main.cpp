1ab2d00dbbc3ed4815a9550e8a4b20a5111f6de5
bibrikthesorcerer <ignatsahovsky@gmail.com>
2023-03-24 22:04:26 +0300

    implemented deterministic automa class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include "det_automa.h"
 
 int main()
 {
-    int aboba = 777;
-    int barabashka = 1335557;
-    int f = 2;
-    //hello bubliki
+   std::vector<state> finite_states(1, state::q3);
+   std::vector<std::vector<state>> trans_table{ {state::q1, state::qe},
+                                                {state::q2, state::qe},
+                                                {state::q2, state::q3},
+                                                {state::q3, state::q2},
+                                                {state::qe, state::qe},
+                                                                     };
+
+   automa1 obj(state::q0, trans_table, finite_states, "input.txt");
+
+   obj.parse();
+
     return 0;
 }
