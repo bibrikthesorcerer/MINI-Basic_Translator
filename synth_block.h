caf41693eb21e905ee0b2c636327cc5a414e09dc
Mikhail <mischanickonow@yandex.ru>
2023-04-28 19:40:40 +0300

    Update synth_block.h

--- synth_block.h
+++ synth_block.h
@@ -8,6 +8,8 @@
 #include <fstream>
 #include <set>
 
+
+
 class SynthSymbol
 {
 public:
@@ -26,16 +28,32 @@ public:
 
     friend bool operator==(const SynthSymbol& obj, const SynthSymbol& obj1);
 
+
 };
 
+class Terminal : public SynthSymbol
+{
+public:
+    Terminal();
 
-class NonTerminal:public SynthSymbol
+    Terminal(const Terminal& obj);
+
+    Terminal(size_t id, std::string name);
+
+    Terminal& operator=(const Terminal& obj);
+
+    friend bool operator<(const Terminal& obj, const Terminal& obj1);
+
+    friend bool operator==(const Terminal& obj, const Terminal& obj1);
+};
+
+class NonTerminal : public SynthSymbol
 {
 public:
     NonTerminal();
     NonTerminal(const NonTerminal& nonTerminal);
     NonTerminal(std::string name, size_t id);
-    
+
     NonTerminal& operator=(const NonTerm& obj);
 
     friend bool operator==(const NonTerminal& obj1, const NonTerminal& obj2);
