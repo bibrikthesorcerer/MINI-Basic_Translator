35848b2c9e0e663068a889059c1f7a740eb2372c
Mikhail <mischanickonow@yandex.ru>
2023-04-28 19:42:05 +0300

    Update synth_block.cpp

--- synth_block.cpp
+++ synth_block.cpp
@@ -34,6 +34,42 @@ SynthSymbol& SynthSymbol::operator=(const SynthSymbol& obj)
 }
 
 
+friend bool Terminal::operator<(const Terminal& obj, const Terminal& obj1)
+{
+    return obj.m_id < obj1.m_id;
+}
+
+friend bool Terminal::operator==(const Terminal& obj, const Terminal& obj1)
+{
+    return obj.m_id == obj1.m_id;
+}
+
+Terminal::Terminal()
+{
+    m_name = "";
+    m_id = -1;
+}
+
+Terminal::Terminal(const Terminal& obj)
+{
+    m_id = obj.m_id;
+    m_name = obj.m_name;
+}
+
+Terminal::Terminal(size_t id, std::string name)
+{
+    m_id = id;
+    m_name = name;
+}
+
+Terminal& Terminal::operator=(const Terminal& obj)
+{
+    m_id = obj.m_id;
+    m_name = obj.m_name;
+    return *this;
+}
+
+
 // NonTerminal class
 NonTerminal::NonTerminal()
 {
