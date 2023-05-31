22dc483cff2af30a66273c83a09ed4ebf8ba50c1
NiKuzD <nik.kuznetsov.04@mail.ru>
2023-05-31 11:00:58 +0300

    Update CF_grammar.cpp

--- SynthBlock/CF_grammar.cpp
+++ SynthBlock/CF_grammar.cpp
@@ -139,3 +139,75 @@ size_t CF_grammar::get_id(std::string S)
 
     return -1;
 }
+
+////////////////////////////////////////////////////////FIRST1//////////////////////////////////////////////////////////////
+std::set<std::shared_ptr<Symbol>> CF_grammar::FIRST1(const std::shared_ptr<Symbol>& S)
+{
+    std::set<Symbol> seen;
+
+    if (dynamic_cast<Terminal*>(S.get()))
+    {
+        std::set<std::shared_ptr<Symbol>> res;
+        res.insert(S);
+        return res;
+    }
+
+
+    return FIRST1_REC(S, seen);
+}
+std::set<std::shared_ptr<Symbol>> CF_grammar::FIRST1_REC(const std::shared_ptr<Symbol>& S, std::set<Symbol>& seen)
+{
+    std::set<std::shared_ptr<Symbol>> first_syms;
+
+    size_t rules_amount = m_rules.count(*S);
+    auto it = m_rules.find(*S);
+
+    for (size_t i = 0; it != m_rules.end() && i < rules_amount; ++i, ++it)
+    {
+        if (dynamic_cast<Terminal*>((*it).second.m_right_part[0].get()))        //Terminal
+            first_syms.insert((*it).second.m_right_part[0]);
+        else if (seen.find(*((*it).second.m_right_part[0])) == seen.end())      //Non_terminal
+        {
+            seen.insert(*((*it).second.m_right_part[0]));
+            first_syms.merge(FIRST1_REC((*it).second.m_right_part[0], seen));
+        }
+    }
+
+    return first_syms;
+}
+
+////////////////////////////////////////////////////////START1//////////////////////////////////////////////////////////////
+std::set<std::shared_ptr<Symbol>> CF_grammar::START1(const std::shared_ptr<Symbol>& S)
+{
+    if (dynamic_cast<Terminal*>(S.get()))
+    {
+        return std::set<std::shared_ptr<Symbol>>();
+    }
+
+    std::set<Symbol> seen;
+
+    return START1_REC(S, seen);
+}
+
+std::set<std::shared_ptr<Symbol>> CF_grammar::START1_REC(const std::shared_ptr<Symbol>& S, std::set<Symbol>& seen)
+{
+    std::set<std::shared_ptr<Symbol>> first_syms;
+
+    size_t rules_amount = m_rules.count(*S);
+    auto it = m_rules.find(*S);
+
+    for (size_t i = 0; it != m_rules.end() && i < rules_amount; ++i, ++it)
+    {
+
+        first_syms.insert(it->second.m_right_part[0]);
+
+        if (dynamic_cast<Non_terminal*>(it->second.m_right_part[0].get()))
+            if (seen.find(*it->second.m_right_part[0]) == seen.end())
+            {
+                seen.insert(*it->second.m_right_part[0]);
+                first_syms.merge(START1_REC((*it).second.m_right_part[0], seen));
+            }
+    }
+
+    return first_syms;
+}
